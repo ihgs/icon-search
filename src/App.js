4224f0c81c5de771c07796c8e7fac1a09c1966a5
ihgs <hgs.dev@gmail.com>
2022-08-18 09:34:24 +0900

    enable to search icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 
-function App() {
+function App(props) {
+
+  const [word, setWord] = useState("");
+  const [searched, setSearched] = useState([]);
+  const iconmap = require("./iconmap.json");
+
+  const filterIcon = (word) => {
+    const targets = [];
+    for (let [ , value] of Object.entries(iconmap)){
+      if ( value.searchwords.indexOf(word.toLowerCase()) > 0) {
+        targets.push(value);
+      }
+    }
+    setSearched(targets);
+    return targets;
+  }
+
+  
+  const search = (event) => {
+    event.preventDefault();
+    filterIcon(word);
+  }
+
+
+
+  const renderIcons = searched.map((data)=>{
+    const firstKey = Object.keys(data.paths)[0];
+    const path = data.paths[firstKey].replaceAll('public/','');
+    return (
+      <li>
+          {data.serviceName}
+           <img src={`${process.env.PUBLIC_URL}/${path}`} alt={firstKey}></img>
+      </li>
+    )
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <form onSubmit={search}>
+
+      <Container>
+        <Row>
+          <Col>
+              <Form.Control type="text" id="searchTxt" value={word} onChange={(event) => setWord(event.target.value)} placeholder="input serach word" />
+          </Col>
+          <Col>
+            <Button type="submit" onClick={search}>Serach</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ul >
+              {renderIcons}
+            </ul>
+            </Col>
+        </Row>
+      </Container>
+      </form>
+      
     </div>
   );
 }
